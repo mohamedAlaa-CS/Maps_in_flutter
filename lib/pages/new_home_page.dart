a9bb88b838143f6540abd6bb08367e51414a818a
Myproject <mohamedAlaa-CS>
2024-02-02 21:02:43 +0200

    zoom level

--- lib/pages/new_home_page.dart
+++ lib/pages/new_home_page.dart
@@ -10,10 +10,19 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   //! intial camera position بتشوف انت عاوز تعرض اي اول ما جوجل ماب تفتح(تفتح علي اي )
+  //? zoom بيحدد درجه الزوم اللي انت محتاجها
+  /* 
+  world view 0 -> 3
+  country view 4 -> 6
+  city view 10 -> 12
+  street view 13 -> 17
+  building view 18 -> 20
+  */
   late CameraPosition initialCameraPosition;
   @override
   void initState() {
     initialCameraPosition = const CameraPosition(
+      zoom: 10,
       target: LatLng(31.0399088096436, 31.38043737066807),
     );
     super.initState();
