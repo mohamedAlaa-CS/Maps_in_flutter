50eda5c82e208c64d09460fad2e6b5d94f01b695
Myproject <mohamedAlaa-CS>
2024-02-02 16:07:06 +0200

    intial camera position

--- lib/pages/new_home_page.dart
+++ lib/pages/new_home_page.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //! intial camera position بتشوف انت عاوز تعرض اي اول ما جوجل ماب تفتح(تفتح علي اي )
+  late CameraPosition initialCameraPosition;
+  @override
+  void initState() {
+    initialCameraPosition = const CameraPosition(
+      target: LatLng(31.0399088096436, 31.38043737066807),
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(31, 41),
-        ),
+        initialCameraPosition: initialCameraPosition,
       ),
     );
   }
