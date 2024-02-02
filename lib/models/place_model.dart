9f5c8802ce8929378b820b53fe99642ecc054914
Myproject <mohamedAlaa-CS>
2024-02-03 00:06:28 +0200

    place model

---
+++ lib/models/place_model.dart
@@ -0,0 +1,28 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class PlaceModel {
+  final int id;
+  final String name;
+  final LatLng latLng;
+
+  PlaceModel({
+    required this.id,
+    required this.name,
+    required this.latLng,
+  });
+
+  List<PlaceModel> places = [
+    PlaceModel(
+        id: 1,
+        name: 'المصريه للاتصالات اولاد صقر',
+        latLng: const LatLng(30.922301998021013, 31.693054430566388)),
+    PlaceModel(
+        id: 2,
+        name: 'مشويات حسني',
+        latLng: const LatLng(31.138226989855006, 31.244260907563444)),
+    PlaceModel(
+        id: 3,
+        name: 'حضتنه الشافعي',
+        latLng: const LatLng(31.022249753995286, 31.32030057071774)),
+  ];
+}
